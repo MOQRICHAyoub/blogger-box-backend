cd1b110b45e2857dc3919ab28b990b144c92a486
MOQRICHAyoub <ayoubmoqrich@gmail.com>
2024-05-26 10:08:38 +0200

    add documentation via Swagger

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -2,12 +2,25 @@ package com.dauphine.blogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blogger box backend",
+				description = "Blogger box endpoints and APIs",
+				contact = @Contact(name = "Ayoub", email = "ayoubmoqrich@gmail.com"),
+				version = "1.0.0"
+		)
+)
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggerBoxBackendApplication.class, args);
 	}
-
 }
+
--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -4,22 +4,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 
 @RestController
+@Tag(
+        name = "Hello World API",
+        description = "My first hello world endpoints"
+)
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
+    @Operation(summary = "Hello world endpoint", description = "Returns 'Hello World!'")
     public String helloWorld() {
         return "Hello World!";
     }
 
     @GetMapping("/hello")
-    public String helloByName(@RequestParam String name) {
+    @Operation(summary = "Hello by name endpoint", description = "Returns 'Hello {name}' by request parameter")
+    public String helloByName(@Parameter(description = "Name to greet") @RequestParam String name) {
         return "Hello " + name;
     }
 
     @GetMapping("/hello/{name}")
-    public String hello(@PathVariable String name) {
+    @Operation(summary = "Hello by name endpoint", description = "Returns 'Hello {name}' by path variable")
+    public String hello(@Parameter(description = "Name to greet") @PathVariable String name) {
         return "Hello " + name;
     }
 }
